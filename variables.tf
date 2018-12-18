323195ebf6b919890d4158297a9a3430254643c3
Peter Lohmann <peter.lohmann@meteogroup.com>
2018-12-18 11:20:07 +0100

    feat: Optionally prefix the table name with `<product>-`

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "product" {
   description = "Name of product / project for which to create the backend resources"
 }
 
+variable "prefix_with_product" {
+  description = "Whether to prepend the lock table name with `<product>-`"
+}
+
 variable "account" {
   description = "Type of AWS account in which to create the backend. Should be either `prod` or `nonprod`"
 }

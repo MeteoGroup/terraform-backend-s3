e21d55d68af2941cc25f6bf32a65dd8c8d1cde75
Robin Schrievers <robin.schrievers@meteogroup.com>
2020-01-15 11:06:57 +0100

    remove prevent destroy temporary to allow for deletion

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ resource "aws_s3_bucket" "terraform_state" {
     enabled = true
   }
 
-  lifecycle {
-    prevent_destroy = true
-  }
+//  lifecycle {
+//    prevent_destroy = true
+//  }
 
   tags = var.tags
 }
